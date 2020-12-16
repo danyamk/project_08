14cf37b2b035b813ace9c276c33099812f5d3768
helpme-kk <71016395+helpme-kk@users.noreply.github.com>
2020-12-16 12:44:00 +0300

    Add files via upload

---
+++ project_8/src/project_8/UnfairWaitList.java
@@ -0,0 +1,14 @@
+package project_8;
+
+public class UnfairWaitList<E> extends WaitList<E> {
+    public UnfairWaitList() { }
+    public void remove(E element) {
+        this.content.remove(element);
+    }
+    public void moveToBack(E element) {
+        if(this.content.remove(element)) {
+            this.content.add(element);
+        }
+    }
+
+}
